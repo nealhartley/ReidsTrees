07cc836c20e79025e0de6a3fcd4d54737a5df78c
nealhartley <neal.w.hartley@gmail.com>
2017-09-28 17:47:02 +1300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { HomeComponent } from './home/home.component';
+
+import {RouterModule, Routes} from '@angular/router';
+
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormControl} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+
+let routes: Routes = [
+  {path: 'testimonials', component: TestimonialsComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'contact', component: ContactUsComponent},
+  {path: '', component: HomeComponent},
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    ServicesComponent,
+    ContactUsComponent,
+    TestimonialsComponent,
+    HomeComponent,
+    HttpModule,
+    ReactiveFormsModule,
+    FormControl
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
