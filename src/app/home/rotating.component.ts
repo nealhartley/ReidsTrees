f687e0b4f244b937bd1a8fbcdddd71173db86d5e
nealhartley <neal.w.hartley@gmail.com>
2017-10-05 18:42:58 +1300

    divs now rotate, overlap between leaving divs and appearing divs gone. Divs move in right direction. TODO: animate movements

--- src/app/home/rotating.component.ts
+++ src/app/home/rotating.component.ts
@@ -11,25 +11,66 @@ import { SERVICES} from '../services';
 export class RotatingComponent {
     services: Service[] = SERVICES;
     index = 0;
+    firstMove = true;
     elements: NodeListOf<Element> = document.getElementsByClassName('rotation_body');
 
     moveLeft(): void {
       console.log('moving_left ' + this.index);
+      this.clearClasses(false, true, true);
       this.elements.item(this.index).classList.add('moveLeft');
-      this.index --;
+      if (this.index === 0) {
+        this.index = this.services.length - 1;
+      } else {
+        this.index --;
+      }
       this.move();
 
     }
 
     moveRight(): void {
-      console.log('moving_right ' + this.index);
+      console.log('moving_right ' + this.index)
+      this.clearClasses(false, true, true);;
       this.elements.item(this.index).classList.add('moveRight');
-      this.index ++;
+      if (this.index === this.services.length - 1) {
+        this.index = 0;
+      } else {
+        this.index ++;
+      }
       this.move();
     }
 
     move(): void {
       console.log('moving ');
+      this.clearClasses(true, false, false);
       this.elements.item(this.index).classList.add('active');
+    }
+
+    // clear classes removes certain classes depending on the booleans handed to it
+    // booleans are named after the classes that will be removed if that bool is true
+    clearClasses(active: boolean, moveLeft: boolean, moveRight: boolean): void {
+      if (active) {
+        console.log(document.getElementsByClassName('active'));
+        document.getElementsByClassName('active')[0].classList.remove('active');
+      }
+      if (!this.firstMove) {
+        if (moveLeft) {
+          console.log(document.getElementsByClassName('moveLeft'));
+
+          try { document.getElementsByClassName('moveLeft')[0].classList.remove('moveLeft');
+          } catch (e) {
+            console.log(e);
+          }
+        }
+
+        if (moveRight) {
+          console.log(document.getElementsByClassName('moveRight'));
+          try { document.getElementsByClassName('moveRight')[0].classList.remove('moveRight');
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      } else {
+        this.firstMove = false;
+      }
     }
 }
