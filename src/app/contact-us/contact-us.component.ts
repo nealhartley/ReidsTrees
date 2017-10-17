cc3492931ad857614ea3d35f7dbee10ee1f43755
nealhartley <neal.w.hartley@gmail.com>
2017-10-17 14:57:06 +1300

    working through form tute on angular.io then will alter to fit sites needs. Need to go fix another site so cut this one short for now

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css']
 })
-export class ContactUsComponent implements OnInit {
+export class ContactUsComponent {
 
-  constructor() { }
+  powers = ['really smart', 'dumb', 'poop', 'ick'];
 
-  ngOnInit() {
+  model = new Hero(18, 'DR DRE', this.powers[2], 'RipperRoo');
+
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
   }
+
+  // TODO remove
+  get diagnostic() { return JSON.stringify(this.model); }
 
 }
