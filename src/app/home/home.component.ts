fa53c97a204b3b2143d4e09ba8b51281f3ee3341
nealhartley <neal.w.hartley@gmail.com>
2017-11-27 19:28:35 +1300

    updated so icons on front page bring in slide out instead of scrolling you down. Added styling to the see more button for the service rotating banner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { RotatingComponent } from './rotating.component';
+import {trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('SlideInOut', [
+      state('in', style({
+        transform: 'translate3d(0,0,0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(120%,0,0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
+
+  menuState: string = 'out';
+  toggleMenu() {
+    // if statement that toggles the value
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
+  }
 
   ngOnInit() {
   }
