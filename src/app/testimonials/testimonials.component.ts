2d8a03620e13d64981ed3f803268052931500c47
nealhartley <neal.w.hartley@gmail.com>
2017-12-11 15:16:40 +1300

    Testimonial page finished for pc

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testimonials.component.css']
 })
 export class TestimonialsComponent implements OnInit {
+  testimonials = [
+    {
+      name : 'Jeannie and John',
+      image: 'jeannie_john.jpg',
+      content: 'Reids Trees provides fantastic service and work. We are happy to say we have found the arborists of our dreams.' +
+      ' We know ouor trees are going to be well looked after for many years to come.'
+    },
+    {
+      name: 'Jenny and Robin',
+      image: 'jenny_robin.jpg',
+      content: 'Our home is our paradise and we needed someone who cared as much for our plants as we do. ' +
+      'Reids Trees has always come through. They leave our property looking one hundred times better every time they leave.'
+    },{
+      name : 'Jeannie and John',
+      image: 'jeannie_john.jpg',
+      content: 'Reids Trees provides fantastic service and work. We are happy to say we have found the arborists of our dreams.' +
+      ' We know ouor trees are going to be well looked after for many years to come.'
+    },
+    {
+      name : 'Jeannie and John',
+      image: 'jeannie_john.jpg',
+      content: 'Reids Trees provides fantastic service and work. We are happy to say we have found the arborists of our dreams.' +
+      ' We know ouor trees are going to be well looked after for many years to come.'
+    },
+    ];
 
   constructor() { }
 
   ngOnInit() {
   }
+
+
+
 
 }
